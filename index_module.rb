81d8f9c871e600f332b1613d96a87758349a8e51
Brandon Murry <brmurry@zappos.com>
2013-07-14 16:35:24 -0700

    added sass and removed default template

--- index_module.rb
+++ index_module.rb
@@ -4,7 +4,7 @@ require 'auth/auth_ext'
 
 class IndexModule < Sinatra::Base
   register Sinatra::Auth
-  
+
   configure do
     internal_config = 'config.yaml'
     external_config = '/etc/brycecube/workouttracker.yaml'
@@ -17,33 +17,33 @@ class IndexModule < Sinatra::Base
     DataMapper::Logger.new($stdout, :debug)
     DataMapper.setup(:default, "#{config['db']['protocol']}://#{config['db']['username']}:#{config['db']['password']}@#{config['db']['host']}/#{config['db']['database']}")
     DataMapper.finalize
-    
+
     set :sessions, true
     use Rack::Session::Cookie, :key => 'rack.session',
                                :expire_after => 31536000,
                                :secret => config[:cookie_secret]
     set :session_secret, config[:cookie_secret]
-    
+
     set :app_name, 'Workout Tracker'
     set :port, config['port']
     set :protection, :except => :json_csrf
   end
-  
+
   before do
-    @js = ['https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', '/js/bootstrap.min.js']
-    @css = ['/css/bootstrap.min.css']
+    @js = ['/js/main.js']
+    @css = ['/css/main.css']
     @nav = [
             { :url => '/',              :label => 'Home',    :child_urls => []},
             { :url => '/account',       :label => 'Profile',      :child_urls => ['account/profile'] },
           ]
   end
-  
+
   get '/' do
-    @css.push( '/css/main.css' )
+    # @css.push( '/css/main.css' )
     @page_title = "#{options.app_name} - Home"
 
     erb :index
   end
-  
+
   run! if app_file == $0
 end
